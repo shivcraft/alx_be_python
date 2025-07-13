21b4f8a7b4677c3f08261f8fdc972186d9155ce9
shivcraft <warrenshivairo@gmail.com>
2025-07-13 21:39:18 +0300

    Add basic operations code

---
+++ python_introduction/basic_operations.py
@@ -0,0 +1,10 @@
+number1 = 10
+ number2 = 5
+sum_result = number1 + number2
+difference = number1 - number2
+product = number1 * number2
+quotient = number1 / number2
+print("Sum:", sum_result)
+print("Difference:", difference)
+print("Product:", product)
+print("Quotient:", quotient)
