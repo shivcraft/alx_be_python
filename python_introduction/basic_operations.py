437ac2aea699d813797914b8d8496a6662569721
shivcraft <warrenshivairo@gmail.com>
2025-07-13 22:42:38 +0300

    Add arithmetic operations to basic_operations.py

--- python_introduction/basic_operations.py
+++ python_introduction/basic_operations.py
@@ -1,13 +1,4 @@
-number1 = 10
- number2 = 5
-sum_result = number1 + number2
-difference = number1 - number2
-product = number1 * number2
-quotient = number1 / number2
-print("Sum:", sum_result)
-print("Difference:", difference)
-print("Product:", product)
-print("Quotient:", quotient)
+
 number1 = 10
 number2 = 5
 sum_result = number1 + number2
@@ -16,4 +7,6 @@ product = number1 * number2
 print(f"Addition of {number1} and {number2} is {sum_result}")
 print(f"Subtraction of {number1} and {number2} is {difference}")
 print(f"Multiplication of {number1} and {number2} is {product}")
-
+Addition of 10 and 5 is 15
+Subtraction of 10 and 5 is 5
+Multiplication of 10 and 5 is 50
